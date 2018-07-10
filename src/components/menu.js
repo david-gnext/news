d3e5362716c6b85cfbf4c0282ae117873389bbb9
david.jaw.hpan@myanmar.gnext.asia <david.jaw.hpan@myanmar.gnext.asia>
2018-07-10 15:15:36 +0630

    add active

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,16 +1,34 @@
 import React from 'react'
 
-const Menu = () => (
-  <nav>
-    <div className="nav-wrapper light-blue lighten-1 row">
-      <ul id="nav-mobile" className="hide-on-med-and-down col m10">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-      <a href="#" className="col m2">Logo</a>
-    </div>
-  </nav>
-)
+class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeName: window.location.pathname
+    }
+  }
+  render() {
+    const menu = ['Sass','News','Technology']
+
+    const isActive = (name) => (
+      this.state.activeName === '/' + name ? 'active' : ''
+    )
+
+    return (
+      <nav>
+        <div className="nav-wrapper light-blue lighten-1 row">
+          <ul id="nav-mobile" className="hide-on-med-and-down col m10">
+            {
+              menu.map((i) => (
+                <li className={isActive(i)} key={i}><a href={'/'+i}>{i}</a></li>
+              ))
+            }
+          </ul>
+          <a href="#" className="col m2 right-align">Logo</a>
+        </div>
+      </nav>
+    )
+  }
+}
 
 export default Menu
