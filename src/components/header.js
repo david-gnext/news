f08e6193a0cc8f66b164e96209a1f12eb9556dc0
Gnext <saw.zin.min.tun@myanmar.gnext.asia>
2018-07-11 10:45:38 +0630

    change header format

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import '../css/header.css';
 const Header = () => (
-  <header className="App-header">
-    <h1 className="App-title">Welcome to React</h1>
+  <header className="App-header blue-grey lighten-4">
+    <div class="row">
+      <div class="col s3">nav</div>
+      <div class="col s6 center-align">News</div>
+      <div class="col s3">Login</div>
+    </div>
   </header>
 )
 
