bb9fa9355f077338e17d07269c73e259b9b47609
Gnext <saw.zin.min.tun@myanmar.gnext.asia>
2018-07-11 12:01:38 +0630

    change class to className

--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,12 @@ import React from 'react'
 import '../css/header.css';
 const Header = () => (
   <header className="App-header blue-grey lighten-4">
-    <div class="row">
-      <div class="col s3">
-        <i class="material-icons">menu</i>
+    <div className="row">
+      <div className="col s3">
+        <i className="material-icons">menu</i>
       </div>
-      <div class="col s6 center-align">News</div>
-      <div class="col s3">Login</div>
+      <div className="col s6 center-align">News</div>
+      <div className="col s3">Login</div>
     </div>
   </header>
 )
